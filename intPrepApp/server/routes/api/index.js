a41c35c35988c7f07bb6e644a6b901acfe9a00f7
Crystal D <crystaldepalma@yahoo.com>
2024-12-28 15:01:13 -0500

    configured api index.js and topicRoutes.js

--- intPrepApp/server/routes/api/index.js
+++ intPrepApp/server/routes/api/index.js
@@ -1,1 +1,16 @@
 // central file to import and configure all API routes
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const protectedRoutes = require('./protectedRoutes');
+const topicRoutes = require('./topicRoutes');
+const anagramRoutes = require('./anagramRoutes');
+const interviewRoutes = require('./interviewRoutes');
+
+// add route models
+router.use('/users', userRoutes);
+router.use('/protected', protectedRoutes);
+router.use('/topics', topicRoutes);
+router.use('/anagrams', anagramRoutes);
+router.use('interview', interviewRoutes);
+
+module.exports = router;
--- intPrepApp/server/routes/api/topicRoutes.js
+++ intPrepApp/server/routes/api/topicRoutes.js
@@ -1,1 +1,22 @@
-// routes for managing topics
+// manage CRUD operations for JavaScript/MERN topics
+const router = require('express').Router();
+const { getAllTopics, getTopicById, createTopic, updateTopic, deleteTopic } = require('../../controllers/topicController');
+const { verifyToken } = require('../../utils/auth');
+
+// public GET all Topics
+router.get('/', getAllTopics);
+
+// public GET a single Topic
+router.get('/:id', getTopicById);
+
+// protected ADD a new Topic
+router.get('/:id, getTopicById');
+
+// protected ADD a new topic
+router.post('/', verifyToken, createTopic);
+
+// protected UPDATE an existing topic
+router.put('/:id', verifyToken, updateTopic);
+
+// protected DELETE a topic
+router.delete('/:id', verifyToken, deleteTopic);
